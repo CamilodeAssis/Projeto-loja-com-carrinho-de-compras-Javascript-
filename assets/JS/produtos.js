e98333773468a86d2f3482e441ec71b529a2966e
CamilodeAssis <camilo.kotecki10@gmail.com>
2022-08-08 13:51:02 -0300

    modificando caminhos

--- assets/JS/produtos.js
+++ assets/JS/produtos.js
@@ -1,10 +1,10 @@
 let prodJson = [
-    {id:1, name:'Camiseta ', img:'images/camisa.png', price:20.19, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Camisa', img:'images/camiseta.png', price:18.00, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Moletom ', img:'images/casaco.png', price:17.45, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Moletom canguru', img:'images/canguru.png', price:19.77, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Jaqueta', img:'images/jaqueta.png', price:21.43, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Sueter', img:'images/sueter.png', price:18.55, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Camisa manga longa', img:'images/mangalonga.png', price:22.36, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Camisa manga longa', img:'images/mangalonga.png', price:22.36, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Camiseta ', img:'assets/images/camisa.png', price:20.19, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Camisa', img:'assets/images/camiseta.png', price:18.00, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Moletom ', img:'assets/images/casaco.png', price:17.45, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Moletom canguru', img:'assets/images/canguru.png', price:19.77, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Jaqueta', img:'assets/images/jaqueta.png', price:21.43, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Sueter', img:'assets/images/sueter.png', price:18.55, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Camisa manga longa', img:'assets/images/mangalonga.png', price:22.36, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Camisa manga longa', img:'assets/images/mangalonga.png', price:22.36, sizes:['S', 'M', 'L'], description:'Descrição do produto em mais de uma linha muito legal bem interessante'}
 ];
